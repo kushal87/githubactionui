4d38e25de902490bc7e293e017c7b4fedff9ff97
kushal <kushal.shah1@ibm.com>
2023-01-24 11:30:46 +0530

    commited sample app

---
+++ GithubPOC/GithubPOC/GithubPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPOCApp.swift
+//  GithubPOC
+//
+//  Created by 003x9k on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
